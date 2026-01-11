c49eecb37c3b5aa729a02a3ad30618c59d847ce2
Hasan Hüseyin Yolcu <89687993+Hasmolam@users.noreply.github.com>
2026-01-11 23:03:45 +0300

    Upload exercise: phplearn/views/404.php

---
+++ Courses/PHP For Beginners/exercises/phplearn/views/404.php
@@ -0,0 +1,12 @@
+<?php require('partials/head.php'); ?>
+
+<?php require('partials/nav.php'); ?>
+
+  <main>
+    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 text-white">
+      <h1 class="text-center text-5xl">404</h1>
+      <h3 class="text-center">Böyle bir sayfa bulunmamaktadır!</h3>
+    </div>
+  </main>
+
+<?php require('partials/footer.php'); ?>
